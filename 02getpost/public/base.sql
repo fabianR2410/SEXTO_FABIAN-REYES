755426cfd411847f8b473c7a96eea3f8f9fff66e
fabianR2410 <fabianrc24@uniandes.edu.ec>
2024-08-01 23:59:54 -0500

    sql

--- 02getpost/public/base.sql
+++ 02getpost/public/base.sql
@@ -1,32 +1,16 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 02-08-2024 a las 05:43:16
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
---
--- Base de datos: `sexto`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `alumnos`
---
-
 CREATE TABLE `alumnos` (
-  `id_alumno` int(11) NOT NULL,
+  `id_Alumno` int(11) NOT NULL,
   `Nombre` text NOT NULL,
   `Apellido` text NOT NULL,
   `Edad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `alumnos`
+--
+
+INSERT INTO `alumnos` (`id_Alumno`, `Nombre`, `Apellido`, `Edad`) VALUES
+(1, 'fabian ', 'reyes', 30);
 
 --
 -- Índices para tablas volcadas
@@ -36,5 +20,15 @@ CREATE TABLE `alumnos` (
 -- Indices de la tabla `alumnos`
 --
 ALTER TABLE `alumnos`
-  ADD PRIMARY KEY (`id_alumno`);
+  ADD PRIMARY KEY (`id_Alumno`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  MODIFY `id_Alumno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
