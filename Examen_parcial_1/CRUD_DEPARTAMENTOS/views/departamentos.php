2c1b13210f7d529f3b08ee9505f1f053311c3fb6
fabianR2410 <fabianrc24@uniandes.edu.ec>
2024-08-15 09:10:04 -0500

    actualizacion

--- Examen_parcial_1/CRUD_DEPARTAMENTOS/views/departamentos.php
+++ Examen_parcial_1/CRUD_DEPARTAMENTOS/views/departamentos.php
@@ -1,6 +1,27 @@
-
 <!DOCTYPE html>
 <html lang="es"></html>
+<?php
+class ClaseConectar
+{public $conexion;
+    protected $db;
+    private $host = "localhost";
+    private $usuario = "root";
+    private $pass = "";
+    private $base = "examenp1";
+    public function ProcedimientoParaConectar()
+    {
+        $this->conexion = mysqli_connect($this->host, $this->usuario, $this->pass, $this->base);
+        mysqli_query($this->conexion, "SET NAMES 'utf8'");
+        if ($this->conexion->connect_error) {
+            die("Error al conectar con el servidor: " . $this->conexion->connect_error);
+        }
+        $this->db = $this->conexion;
+        if ($this->db == false) die("Error al conectar con la base de datos: " . $this->conexion->connect_error);
+
+        return $this->conexion;
+    }
+}
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
