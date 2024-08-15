3eb9d2561db031fc7af3939be2a13bad9afc6290
fabianR2410 <fabianrc24@uniandes.edu.ec>
2024-08-14 23:46:15 -0500

    actualizacion 23:46

--- SEMANA2_TAREA1_REYES_FABIAN/ccrud cliente/public/base.sql
+++ SEMANA2_TAREA1_REYES_FABIAN/ccrud cliente/public/base.sql
@@ -1,87 +1,64 @@
--- MySQL Workbench Forward Engineering
+--- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema debersemana02
+-- Schema Examenp1
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema debersemana02
+-- Schema Examenp1
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `debersemana02` DEFAULT CHARACTER SET utf8 ;
-USE `debersemana02` ;
+CREATE SCHEMA IF NOT EXISTS `Examenp1` DEFAULT CHARACTER SET utf8 ;
+USE `Examenp1` ;
 
 -- -----------------------------------------------------
--- Table `debersemana02`.`Clientes`
+-- Table `Examenp1`.`Departamentos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `debersemana02`.`Clientes` (
-  `Idcliente` INT NULL AUTO_INCREMENT,
-  `Nombre` VARCHAR(100) NOT NULL,
-  `Apellido` VARCHAR(100) NOT NULL,
-  `Correo` VARCHAR(100) NOT NULL,
-  `Telefono` VARCHAR(15) NOT NULL,
-  `Direccion` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`Idcliente`),
-  UNIQUE INDEX `Correo_UNIQUE` (`Correo` ASC) )
+CREATE TABLE IF NOT EXISTS `Examenp1`.`Departamentos` (
+  `idDepartamentos` INT(8) NOT NULL AUTO_INCREMENT,
+  `Nombre_depeartamentos` TEXT NOT NULL,
+  `Ubicacion` TEXT NOT NULL,
+  `Jefe_departamento` TEXT NOT NULL,
+  `Extencion` TEXT NOT NULL,
+  PRIMARY KEY (`idDepartamentos`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `debersemana02`.`Pedido`
+-- Table `Examenp1`.`Emplados`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `debersemana02`.`Pedido` (
-  `idPedido` INT NULL AUTO_INCREMENT,
-  `idCliente` INT NOT NULL,
-  `FechaPedido` DATE NOT NULL,
-  `Total` DECIMAL(10,2) NOT NULL,
-  `Clientes_Idcliente` INT NOT NULL,
-  PRIMARY KEY (`idPedido`),
-  INDEX `fk_Pedidos_Clientes_idx` (`Clientes_Idcliente` ASC) ,
-  CONSTRAINT `fk_Pedidos_Clientes`
-    FOREIGN KEY (`Clientes_Idcliente`)
-    REFERENCES `debersemana02`.`Clientes` (`Idcliente`)
+CREATE TABLE IF NOT EXISTS `Examenp1`.`Emplados` (
+  `idEmplados` INT(10) NOT NULL AUTO_INCREMENT,
+  `Nombre` TEXT NOT NULL,
+  `Apellido` TEXT NOT NULL,
+  `Correo` TEXT NOT NULL,
+  `Telefono` TEXT NOT NULL,
+  PRIMARY KEY (`idEmplados`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Examenp1`.`Asignaciones`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Examenp1`.`Asignaciones` (
+  `idAsignacion` INT(8) NOT NULL AUTO_INCREMENT,
+  `idEmplados` INT(10) NOT NULL,
+  `idDepartamentos` INT(8) NOT NULL,
+  `Fecha_asignacion` DATE NOT NULL,
+  PRIMARY KEY (`idAsignacion`, `idEmplados`, `idDepartamentos`),
+  INDEX `fk_Emplados_has_Departamentos_Departamentos1_idx` (`idDepartamentos` ASC) ,
+  INDEX `fk_Emplados_has_Departamentos_Emplados_idx` (`idEmplados` ASC) ,
+  CONSTRAINT `fk_Emplados_has_Departamentos_Emplados`
+    FOREIGN KEY (`idEmplados`)
+    REFERENCES `Examenp1`.`Emplados` (`idEmplados`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `debersemana02`.`Detalles`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `debersemana02`.`Detalles` (
-  `idDetalle` INT NULL,
-  `idPedido` INT NOT NULL,
-  `idProductos` INT NOT NULL,
-  `Cantidad` INT NOT NULL,
-  `PrecioUnitario` DECIMAL(10,2) NOT NULL,
-  `Pedido_idPedido` INT NOT NULL,
-  PRIMARY KEY (`idDetalle`),
-  INDEX `fk_DetallePedido_Pedido1_idx` (`Pedido_idPedido` ASC) ,
-  CONSTRAINT `fk_DetallePedido_Pedido1`
-    FOREIGN KEY (`Pedido_idPedido`)
-    REFERENCES `debersemana02`.`Pedido` (`idPedido`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `debersemana02`.`Productos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `debersemana02`.`Productos` (
-  `idProductos` INT NULL AUTO_INCREMENT,
-  `NombreProducto` VARCHAR(100) NOT NULL,
-  `Descripcion` TEXT NULL,
-  `Precio` DECIMAL(10,2) NOT NULL,
-  `Pedido_idPedido` INT NOT NULL,
-  PRIMARY KEY (`idProductos`),
-  INDEX `fk_Productos_Pedido1_idx` (`Pedido_idPedido` ASC) ,
-  CONSTRAINT `fk_Productos_Pedido1`
-    FOREIGN KEY (`Pedido_idPedido`)
-    REFERENCES `debersemana02`.`Pedido` (`idPedido`)
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Emplados_has_Departamentos_Departamentos1`
+    FOREIGN KEY (`idDepartamentos`)
+    REFERENCES `Examenp1`.`Departamentos` (`idDepartamentos`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -90,3 +67,4 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
