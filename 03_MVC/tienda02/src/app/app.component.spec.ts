c7f254789403cd8e2414f90a75d52aa4ae822d20
fabianR2410 <fabianrc24@uniandes.edu.ec>
2024-08-22 21:17:45 -0500

    TIENDA

--- 03_MVC/tienda02/src/app/app.component.spec.ts
+++ 03_MVC/tienda02/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tienda' title`, () => {
+  it(`should have the 'tienda02' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tienda');
+    expect(app.title).toEqual('tienda02');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tienda');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tienda02');
   });
 });
